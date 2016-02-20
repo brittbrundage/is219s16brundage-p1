fb30f91e2066cd46a59e43ad7ab49456fc6c282f
Brittani <bmb23@njit.edu>
2016-02-19 22:47:59 -0500

    Experience picture update

--- index.php
+++ index.php
@@ -197,7 +197,7 @@
                    June 2012 - September 2013  </p>
                 </div>
                 <div class="col-lg-5 col-sm-pull-6  col-sm-6">
-                    <img class="img-responsive" src="img/experience.png" alt="">
+                    <img class="img-responsive" src="img/work.png" alt="">
                 </div>
             </div>
 
